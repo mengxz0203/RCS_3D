65327aad08fba061df7157ba334eee8aec6b4592
mengxz0203 <1196797300@qq.com>
2020-05-17 17:57:18 +0800

    screen

--- code/config.py
+++ code/config.py
@@ -1,18 +1,48 @@
 import os
 from os import path
 
+root = path.join(path.dirname(path.dirname(__file__)))
+print(root)
+
 
 class FilePath(object):
 
     # 模板图片绝对路径
     def template_image_dir(self):
-        image_dir = path.join(os.getcwd(), 'template/')
+        image_dir = path.join(root, 'images/template/')
+        print("path:", image_dir)
         if not os.path.exists(image_dir):
             os.makedirs(image_dir)
         return image_dir
 
+    # 屏幕区域图片绝对路径
     def screen_image_dir(self):
-        image_dir = path.join(os.getcwd(), 'screen/')
+        image_dir = path.join(root, 'images/screen/')
         if not os.path.exists(image_dir):
             os.makedirs(image_dir)
         return image_dir
+
+    # 原始图片路径
+    def origin_image_dir(self):
+        image_dir = path.join(root, 'images/origin')
+        if not os.path.exists(image_dir):
+            os.makedirs(image_dir)
+        return image_dir
+
+    # 标定图片（左）
+    def chessboard_image_left_dir(self):
+        image_dir = path.join(root, 'images/left')
+        if not os.path.exists(image_dir):
+            os.makedirs(image_dir)
+        return image_dir
+
+    # 标定图片（右）
+    def chessboard_image_right_dir(self):
+        image_dir = path.join(root, 'images/right')
+        if not os.path.exists(image_dir):
+            os.makedirs(image_dir)
+        return image_dir
+
+
+# 测试
+FilePath.template_image_dir(self=None)
--- code/get_img.py
+++ code/get_img.py
@@ -2,24 +2,28 @@ import os
 import cv2
 from config import FilePath
 
+
 class Image(object):
     def __init__(self,
                  template_image,
-                 screen_image,
+                 screen_image
                  ):
         self.template_image_path = self.get_template_path(template_image)
-
+        self.screen_image_path = self.get_screen_path(screen_image)
 
     # 获取模板图片路径
     def get_template_path(self, template_image):
         template_dir = FilePath.template_image_dir()
-        self.match_exist_image((template_dir,template_image))
+        self.match_exist_image(template_dir, template_image)
         template_path = '{}/{}'.format(template_dir, template_image)
         return template_path
 
     # 获取屏幕区域图片路径
     def get_screen_path(self, screen_image):
-
+        screen_dir = FilePath.screen_image_dir()
+        self.match_exist_image(screen_dir, screen_image)
+        screen_path = '{}/{}'.format(screen_dir, screen_image)
+        return screen_path
 
     # 检测图片是否存在
     def match_exist_image(self, dir_path, image):
@@ -27,3 +31,8 @@ class Image(object):
             open(dir_path + '/' + image)
         except FileNotFoundError as e:
             raise FileNotFoundError(e)
+
+
+template = 'qq.png'
+screen = '1.jpg'
+Image.get_screen_path(template)
